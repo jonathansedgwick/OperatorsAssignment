c4277ec562c8f5126521ee2b713f8a42d48fe7d9
jonathansedgwick <111292676+jonathansedgwick@users.noreply.github.com>
2022-08-22 08:35:21 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var var1 = 10
+    var var2 = 5
+    println("$var1 + $var2 = " + (var1 + var2))
+    println("$var1 - $var2 = " + (var1 - var2))
+    println("$var1 * $var2 = " + (var1 * var2))
+    println("$var1 / $var2 = " + (var1 / var2))
+    println("$var1 % $var2 = " + (var1 % var2))
+    println("$var1 equals $var2 = " + (var1 == var2))
+}
